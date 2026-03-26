44e9e44ed870c10a924aeddc58613c1e8d9e4eca
SamCorp <Manhcuong5311@gmail.com>
2026-03-26 09:39:43 +0700

    Initial Commit

---
+++ WakeLock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WakeLock
+//
+//  Created by Sam Manh Cuong on 26/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WakeLock/WakeLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeLockApp.swift
+//  WakeLock
+//
+//  Created by Sam Manh Cuong on 26/3/26.
+//
+
+import SwiftUI
+
+@main
+struct WakeLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
